ae8d944a87ac998efd8f26a8257cc98280f0043c
Hariharan Mudaliar <hrhn.mudaliar251@gmail.com>
2025-06-14 00:19:00 +0530

    Update requirements.txt

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Navigate to Django project directory
 cd collaborative_forms
 
-python manage.py collectstatic --noinput
+# Create static directory if it doesn't exist
+mkdir -p static
+
+# Create migrations in correct order
+python manage.py makemigrations accounts
+python manage.py makemigrations forms
+python manage.py makemigrations
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Go back to root for deployment
+cd ..
